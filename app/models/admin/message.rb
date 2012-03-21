c0e1ef993308202ce054a52c7ac636220360a2dd
zhuzhe <zhuzheiswho@sina.com>
2012-03-21 10:36:52 +0800

    dev

--- app/models/admin/message.rb
+++ app/models/admin/message.rb
@@ -2,7 +2,7 @@ class Admin::Message < ActiveRecord::Base
     set_table_name 'messages'
 
     def get_title
-    	self.title || '无主题'
+    	self.title.blank? ? '无主题' : self.title
     end
     
 end
